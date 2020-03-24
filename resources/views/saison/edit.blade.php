145815166a50716ef28a4e2e3660ae0fe83f2a08
RedaBt <38280336+RedaBt@users.noreply.github.com>
2020-03-24 16:10:53 +0100

    First Commit4

--- resources/views/saison/edit.blade.php
+++ resources/views/saison/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Update Ville</div>
+                <div class="card-header">Update Saison</div>
                 <div class="panel-body">
 
 
